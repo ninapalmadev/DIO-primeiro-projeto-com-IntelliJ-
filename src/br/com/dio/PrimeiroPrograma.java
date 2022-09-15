4c0f9af0c64ca00e0b83cad358ef8e3b0b5a2028
Nina Palma <ninapalmadev@gmail.com>
2022-09-15 11:20:29 -0300

    Repositório com primeiro código Java usando IntelliJ

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+    Gato gato = new Gato();
+    Livros livro = new Livros();
+
+        System.out.println(gato);
+        System.out.println(livro);
+        /*int a = 5;
+        int b = 10;
+
+        System.out.println("Hello World " + (a+b));*/
+    }
+}
+
+class Livros{
+    private String nome;
+    private int pags;
+}
